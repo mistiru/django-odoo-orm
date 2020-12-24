3b9df2b11d8e08d4220a0ea13717a869b25b4978
mistiru <dev@mistiru.me>
2020-12-24 08:12:30 +0100

    Add setting to disable connect at startup

--- django_odoo_orm/apps.py
+++ django_odoo_orm/apps.py
@@ -1,12 +1,12 @@
 from django.apps import AppConfig
 from odoo_orm.connection import OdooConnection
 
-from django_odoo_orm.settings import ODOO_CONNECTION
+from django_odoo_orm.settings import ODOO_CONNECTION, ODOO_CONNECTION_CONNECT_AT_STARTUP
 
 
 class DjangoOdooOrm(AppConfig):
     name = 'django_odoo_orm'
 
     def ready(self):
-        odoo = OdooConnection.get_connection()
-        odoo.connect(**{k.lower(): v for k, v in ODOO_CONNECTION.items()})
+        if ODOO_CONNECTION_CONNECT_AT_STARTUP:
+            OdooConnection.get_connection().connect(**{k.lower(): v for k, v in ODOO_CONNECTION.items()})
